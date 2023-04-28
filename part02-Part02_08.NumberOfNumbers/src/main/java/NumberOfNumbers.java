c4ed6df3dbf7afc83e768b32895b26435101e7b2
Leroy <66456764+leroytsang@users.noreply.github.com>
2023-04-28 14:23:27 -0600

    Upload part02_06 to 10

---
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class NumberOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            } else {
+                number += 1;
+            }
+        }
+        System.out.println("Number of numbers: " + number);
+    }
+}
---
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOfNegatives = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            if (input <= 0) {
+                numOfNegatives += 1;
+            }
+        }
+        System.out.println("Number of negative numbers: " + numOfNegatives);
+    }
+}
